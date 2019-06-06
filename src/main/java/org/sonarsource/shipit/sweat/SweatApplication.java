cf72094e3b60c7baceaabff4a889eba676050969
Andrei Epure <andrei.epure@sonarsource.com>
2019-06-06 14:23:59 +0200

    Boilerplate code

---
+++ src/main/java/org/sonarsource/shipit/sweat/SweatApplication.java
@@ -0,0 +1,13 @@
+package org.sonarsource.shipit.sweat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SweatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SweatApplication.class, args);
+	}
+
+}
